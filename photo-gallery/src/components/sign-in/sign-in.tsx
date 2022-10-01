50f0da0d3120fca34006255eb47eece96919cc60
Jason Wisniewski <87104768+JasonWisniewski@users.noreply.github.com>
2022-10-01 13:49:46 -0400

    sign in page ui sign up ui

--- photo-gallery/src/components/sign-in/sign-in.tsx
+++ photo-gallery/src/components/sign-in/sign-in.tsx
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled';
 import React from "react";
-import {IonButton} from "@ionic/react";
+import {IonButton, IonContent, IonInput, IonItem, IonLabel, IonList} from "@ionic/react";
 
 export interface SignInProps {
   setShowModal: (showModal: boolean) => void;
@@ -9,22 +9,36 @@ export interface SignInProps {
 const SignIn = ({setShowModal}: SignInProps) => {
   return (
     <>
-      <div>Sign yo ass in fool</div>
-      <IonButton onClick={() => {
-        //making it in here but state not changing
-        console.log('made it to onclick')
-        setShowModal(false)
-      }}
-                 style=
-                   {{
-                   padding: '10px',
-                   margin: '0',
-                   height: '40px',
-                   width: '40px'
-                 }}
-      >
-        X
-      </IonButton>
+      <IonContent>
+
+        <div>Sign yo ass in fool</div>
+        <IonButton onClick={() => {
+          setShowModal(false)
+        }}
+                   style=
+                     {{
+                       padding: '10px',
+                       margin: '0',
+                       height: '40px',
+                       width: '40px'
+                     }}
+        >
+          X
+        </IonButton>
+        <IonList>
+          <IonItem>
+            <IonLabel position="stacked">Email</IonLabel>
+            <IonInput value=''> </IonInput>
+          </IonItem>
+          <IonItem>
+            <IonLabel position="stacked">Password</IonLabel>
+            <IonInput value=''> </IonInput>
+          </IonItem>
+          <IonItem>
+            <IonLabel position="stacked">Don't have an account? Signup!</IonLabel>
+          </IonItem>
+        </IonList>
+      </IonContent>
     </>
   )
 }
--- photo-gallery/src/components/sign-up/sign-up.tsx
+++ photo-gallery/src/components/sign-up/sign-up.tsx
@@ -1,10 +1,53 @@
 import styled from '@emotion/styled';
 import React from "react";
+import {IonButton, IonContent, IonInput, IonItem, IonLabel, IonList} from "@ionic/react";
 
-const SignUp: React.FC = () => {
+export interface SignUpProps {
+  setShowModal: (showModal: boolean) => void;
+}
+
+const SignUp = ({setShowModal}: SignUpProps) => {
   return (
     <>
-      <div>yay!</div>
+      <IonContent>
+        <div>Sign up for a account fool</div>
+        <IonButton onClick={() => {
+          setShowModal(false)
+        }}
+                   style=
+                     {{
+                       padding: '10px',
+                       margin: '0',
+                       height: '40px',
+                       width: '40px'
+                     }}
+        >
+          X
+        </IonButton>
+        <IonList>
+          <IonItem>
+            <IonLabel position="stacked">User Name</IonLabel>
+            <IonInput value=''> </IonInput>
+          </IonItem>
+          <IonItem>
+            <IonLabel position="stacked">Email</IonLabel>
+            <IonInput value=''> </IonInput>
+          </IonItem>
+          <IonItem>
+            <IonLabel position="stacked">Password</IonLabel>
+            <IonInput value=''> </IonInput>
+          </IonItem>
+          <IonItem>
+            <IonLabel position="stacked">Confirm Password</IonLabel>
+            <IonInput value=''> </IonInput>
+          </IonItem>
+          <IonItem>
+            {/*<IonButton OnClick={() => console.log('click')}>*/}
+            {/*  <IonLabel position="stacked">Don't have an account? Signup!</IonLabel>*/}
+            {/*</IonButton>*/}
+          </IonItem>
+        </IonList>
+      </IonContent>
     </>
   )
 }
